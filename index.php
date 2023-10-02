caa06f78394d2ce656a30d040429668bddcb4018
Chris Ware <chris.ware@morphsites.com>
2023-10-02 19:15:18 +0100

    remove print_r

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $inputArray = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5];
 
 // #3 - Multidimensional. Please recursively reverse to a max depth of 2 child levels
 $inputArray = ['a' => [1, 2, 3, 'a' => ['a', 'b', 'c' => ['a', 'b', 'c', 'd' => [1, 2, 3]]]], 2, 'c' => 3, 4, 5];
-print_r(arrayReverse($inputArray, maxDepth: 2));
+//print_r(arrayReverse($inputArray, maxDepth: 2));
 
 /**
  * Full PHPUnit tests are in place should you wish to verify before submitting
