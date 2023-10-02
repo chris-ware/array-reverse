9c65ac760c95b54e374ee8355cdd66501d7547d6
Chris Ware <chris.ware@morphsites.com>
2023-10-02 20:08:12 +0100

    fix: Fix index.php to use moved funtion

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 declare(strict_types = 1);
 
+use Src\Arr;
+
 require_once 'vendor/autoload.php';
 
 // #1 - Indexed array
 $inputArray = [1, 2, 3, 4, 5];
-print_r(arrayReverse($inputArray));
+print_r(Arr::reverse($inputArray));
 
 // #2 - Associative array
 $inputArray = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5];
-print_r(arrayReverse($inputArray));
+print_r(Arr::reverse($inputArray));
 
 // #3 - Multidimensional. Please recursively reverse to a max depth of 2 child levels
 $inputArray = ['a' => [1, 2, 3, 'a' => ['a', 'b', 'c' => ['a', 'b', 'c', 'd' => [1, 2, 3]]]], 2, 'c' => 3, 4, 5];
-print_r(arrayReverse($inputArray, maxDepth: 2));
+print_r(Arr::reverse($inputArray, maxDepth: 2));
